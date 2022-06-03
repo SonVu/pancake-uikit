aae1d80bc544dda2b8bbc853a9422c25ca1ef9e1
sonvu.ae <sonvu.ae@gmail.com>
2022-06-03 18:34:22 +0700

    fix: Token

--- dist/components/Svg/index.d.ts
+++ dist/components/Svg/index.d.ts
@@ -47,5 +47,7 @@ export { default as SearchIcon } from "./Icons/Search";
 export { default as SwapVertIcon } from "./Icons/SwapVert";
 export { default as SyncAltIcon } from "./Icons/SyncAlt";
 export { default as WarningIcon } from "./Icons/Warning";
+export { default as TuneIcon } from "./Icons/TuneIcon";
+export { default as HistoryIcon } from "./Icons/HistoryIcon";
 export { default as Svg } from "./Svg";
 export type { SvgProps } from "./types";
